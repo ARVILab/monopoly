rework policies, add dqn optimizer, fix gradient graph, rename

main.py
@@ -1,8 +1,7 @@
 from trainer import Trainer
 from nn_wrapper import NNWrapper
-from arena import Arena
-from policies.random import RandomAgent
-from policies.fixed import FixedAgent
+from utils.storage_ppo import StoragePPO
+from utils.storage_dqn import StorageDQN
 import config
 
 import torch
@@ -20,15 +19,13 @@ def main():
     print('device', config.device)
 
     if args.model == 'init':
-        policy = NNWrapper(config.state_space, config.action_space)
-        policy.call_counter = 0
+        policy = NNWrapper('actor_critic', config.state_space, config.action_space)
         policy.to(config.device)
     else:
         policy = torch.load('./models/model.pt')
-        
-    policy.call_counter = 0
 
-    trainer = Trainer(policy, n_episodes=5000, n_games_per_eps=1, n_rounds=5000, n_eval_games=10, verbose_eval=20,
+    storage_class = StoragePPO
+    trainer = Trainer(policy, storage_class=storage_class, n_episodes=5000, n_games_per_eps=50, n_rounds=5000, n_eval_games=10, verbose_eval=20,
                       checkpoint_step=5, reset_files=True)
     start = datetime.datetime.now()
     trainer.run()

monopoly/player.py
@@ -1,5 +1,4 @@
 import config
-from utils.storage import Storage
 
 import numpy as np
 import logging
@@ -26,6 +25,9 @@ class Player:
 
         self.mortgages = []
         self.buyings = []
+        self.last_state = None
+        self.last_reward = None
+        self.game = None
 
         self.obligatory_acts = {
             'Idle': None,
@@ -54,19 +56,12 @@ class Player:
 
     def set_game(self, game, n_game):
         self.game = game
-
-        if n_game == 0:
-            obs = self.game.get_state(self)
-            self.storage.add_init_obs(obs, step=0)
-            self.storage.to(self.device)
-
         self.last_state = self.game.get_state(self)
         self.last_reward = game.get_reward(self, self.last_state)
 
     def reset_mortgage_buy(self):
         self.mortgages = []
         self.buyings = []
-
 
     def act(self, space):
         space_type = space.get_type()
@@ -77,7 +72,6 @@ class Player:
 
         if not self.is_bankrupt:
             self.optional_actions()
-
 
     def move(self, roll):
         old_position = self.position
@@ -156,13 +150,14 @@ class Player:
         action_mask_gpu = torch.FloatTensor(action_mask).to(self.device)
 
         state = self.game.get_state(self)
-        value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu)
+        with torch.no_grad():
+            value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu)
 
-        action_item = action.item()
-        if action_item >= 29 and action_item <= 56:
-            self.mortgages.append(action_item)
-        if action_item >= 1 and action_item <= 28:
-            self.buyings.append(action_item)
+        # action_item = action.item()
+        # if action_item >= 29 and action_item <= 56:
+        #     self.mortgages.append(action_item)
+        # if action_item >= 1 and action_item <= 28:
+        #     self.buyings.append(action_item)
 
         do_nothing = self.apply_action(action)
 
@@ -174,8 +169,8 @@ class Player:
         next_state = self.game.get_state(self)
         reward = self.game.get_reward(self, next_state)
 
-        if not self.obs_equals(self.last_state, state) or not self.obs_equals(self.last_reward, reward):
-            self.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+        if not self.obs_equals(self.last_state, state) or not self.reward_equals(self.last_reward, reward):
+            self.storage.push(state, action, action_log_prob, value, reward, [1.0])
 
     def get_bid(self, max_bid, org_price, state):
         do_nothing, bid = self.policy.auction_policy(max_bid, org_price, state, self.cash)
@@ -189,7 +184,8 @@ class Player:
         action_mask_gpu = torch.FloatTensor(action_mask).to(self.device)
 
         state = self.game.get_state(self)
-        value, action, action_log_prob = self.policy.jail_policy(state, self.cash, action_mask_gpu)
+        with torch.no_grad():
+            value, action, action_log_prob = self.policy.jail_policy(state, self.cash, action_mask_gpu)
 
         do_nothing = self.apply_action(action)
         if do_nothing:
@@ -202,8 +198,8 @@ class Player:
                     next_state = self.game.get_state(self)
                     reward = self.game.get_reward(self, next_state)
 
-                    if not self.obs_equals(self.last_state, state) or not self.obs_equals(self.last_reward, reward):
-                        self.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+                    if not self.obs_equals(self.last_state, state) or not self.reward_equals(self.last_reward, reward):
+                        self.storage.push(state, action, action_log_prob, value, reward, [1.0])
                     return False # means not staying in jail
 
             if config.verbose['stay_in_jail']:
@@ -212,15 +208,15 @@ class Player:
             next_state = self.game.get_state(self)
             reward = self.game.get_reward(self, next_state)
 
-            if not self.obs_equals(self.last_state, state) or not self.obs_equals(self.last_reward, reward):
-                self.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+            if not self.obs_equals(self.last_state, state) or not self.reward_equals(self.last_reward, reward):
+                self.storage.push(state, action, action_log_prob, value, reward, [1.0])
             return True # staying in jail
 
         next_state = self.game.get_state(self)
         reward = self.game.get_reward(self, next_state)
 
-        if not self.obs_equals(self.last_state, state) or not self.obs_equals(self.last_reward, reward):
-            self.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+        if not self.obs_equals(self.last_state, state) or not self.reward_equals(self.last_reward, reward):
+            self.storage.push(state, action, action_log_prob, value, reward, [1.0])
         return False # means he paid or used card
 
 
@@ -235,21 +231,22 @@ class Player:
             action_mask_gpu = torch.FloatTensor(action_mask).to(self.device)
 
             state = self.game.get_state(self)
-            value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu, self.mortgages, self.buyings)
+            with torch.no_grad():
+                value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu, self.mortgages, self.buyings)
 
-            action_item = action.item()
-            if action_item >= 29 and action_item <= 56:
-                self.mortgages.append(action_item)
-            if action_item >= 1 and action_item <= 28:
-                self.buyings.append(action_item)
+            # action_item = action.item()
+            # if action_item >= 29 and action_item <= 56:
+            #     self.mortgages.append(action_item)
+            # if action_item >= 1 and action_item <= 28:
+            #     self.buyings.append(action_item)
 
             do_nothing = self.apply_action(action)
 
             next_state = self.game.get_state(self)
             reward = self.game.get_reward(self, next_state)
 
-            if not self.obs_equals(self.last_state, state) or not self.obs_equals(self.last_reward, reward):
-                self.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+            if not self.obs_equals(self.last_state, state) or not self.reward_equals(self.last_reward, reward):
+                self.storage.push(state, action, action_log_prob, value, reward, [1.0])
 
             if do_nothing:
                 break
@@ -258,9 +255,8 @@ class Player:
         try:
             action_item = action.item()
         except:
-            with open('error.csv', 'w') as f:
-                print(str(action))
-            action_item = 0
+            print(action)
+            raise Exception('WTF')
 
         if action_item == 0:
             return True # do nothing
@@ -631,7 +627,8 @@ class Player:
                         action_mask_gpu = torch.FloatTensor(action_mask).to(creditor.device)
 
                         state = creditor.game.get_state(creditor)
-                        value, action, action_log_prob = creditor.policy.act(state, creditor.cash, action_mask_gpu)
+                        with torch.no_grad():
+                            value, action, action_log_prob = creditor.policy.act(state, creditor.cash, action_mask_gpu)
 
                         do_nothing = creditor.apply_action(action)
 
@@ -639,7 +636,7 @@ class Player:
                         reward = creditor.game.get_reward(creditor, next_state)
 
                         if not creditor.obs_equals(creditor.last_state, state) or not creditor.obs_equals(creditor.last_reward, reward):
-                            creditor.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+                            creditor.storage.push(state, action, action_log_prob, value, reward, [1.0])
                     creditor.update_rent(space)
                 if went_bankrupt:
                     break
@@ -661,14 +658,15 @@ class Player:
         action_mask_gpu = torch.FloatTensor(action_mask).to(self.device)
 
         state = self.game.get_state(self)
-        value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu)
+        with torch.no_grad():
+            value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu)
 
         reward = self.game.get_reward(self, state)
 
         mask = [0.0]
 
-        if not self.obs_equals(self.last_state, state) or not self.obs_equals(self.last_reward, reward):
-            self.storage.insert(state, action, action_log_prob, value, reward, mask)
+        if not self.obs_equals(self.last_state, state) or not self.reward_equals(self.last_reward, reward):
+            self.storage.push(state, action, action_log_prob, value, reward, mask)
 
     def pay_bank_interest(self, space):
         bank_interest = space.price * 0.1
@@ -699,14 +697,15 @@ class Player:
             action_mask_gpu = torch.FloatTensor(action_mask).to(self.device)
 
             state = self.game.get_state(self)
-            value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu)
+            with torch.no_grad():
+                value, action, action_log_prob = self.policy.act(state, self.cash, action_mask_gpu)
 
             do_nothing = self.apply_action(action)
 
             next_state = self.game.get_state(self)
             reward = self.game.get_reward(self, next_state)
 
-            self.storage.insert(state, action, action_log_prob, value, reward, [1.0])
+            self.storage.push(state, action, action_log_prob, value, reward, [1.0])
             if do_nothing:
                 break
 
@@ -725,7 +724,7 @@ class Player:
 
 
     def reward_wealth(self):
-        total_wealth = self.cash * 0.5
+        total_wealth = self.cash * 0.1
         for key in self.properties:
             for space in self.properties[key]:
                 space_cost = 0

show_match.py
@@ -32,7 +32,7 @@ def main():
         policy = NNWrapper(config.state_space, config.action_space)
         policy.to(config.device)
     else:
-        model_name = 'model-{}.pt'.format(np.abs(model))
+        model_name = 'model-{}.pt'.format(np.abs(args.model))
         print('Loading model:', model_name)
         if config.device.type == 'cpu':
             policy = torch.load(os.path.join('./models', model_name), map_location=lambda storage, loc: storage)

trainer.py
@@ -3,17 +3,17 @@ import sys
 import logging
 import os
 import numpy as np
+from tqdm import tqdm
 
 import torch
 
 from policies.fixed import FixedAgent
 from policies.random import RandomAgent
 from arena import Arena
-from optimizers.ppo import PPO
+from optimizers.ppo_optimizer import PPO
 from monopoly.player import Player
 import config
 from monopoly.game import Game
-from utils.storage import Storage
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 class Trainer(object):
 
-    def __init__(self, policy, n_episodes=100, n_games_per_eps=10, n_rounds=200, n_eval_games=50, verbose_eval=50, checkpoint_step=10, reset_files=True):
+    def __init__(self, policy, storage_class, n_episodes=100, n_games_per_eps=10, n_rounds=200, n_eval_games=50, verbose_eval=50, checkpoint_step=10, reset_files=True):
         self.policy = policy
         self.n_games = n_games_per_eps
         self.n_rounds = n_rounds
@@ -29,9 +29,10 @@ class Trainer(object):
         self.n_eval_games = n_eval_games
         self.checkpoint_step = checkpoint_step
         self.device = config.device
+        self.storage_class = storage_class
 
         self.episodes = n_episodes
-        self.learning_rate = 1e-3
+        self.learning_rate = 1e-4
         self.clip_param = 0.2
         self.value_loss_coef = 0.5
         self.entropy_coef = 0.01
@@ -41,11 +42,10 @@ class Trainer(object):
         self.gae_coef = 0.95
         self.ppo_epoch = 10
         self.epsilon = 1e-8
-        self.n_mini_batch = 4
+        self.mini_batch_size = 2048
 
-        self.agent = PPO(self.policy, self.clip_param, self.ppo_epoch, self.n_mini_batch,
-                         self.value_loss_coef, self.entropy_coef, self.learning_rate, self.epsilon,
-                         self.max_grad_norm)
+        self.optimizer = PPO(self.policy, self.clip_param, self.ppo_epoch, self.mini_batch_size,
+                             self.value_loss_coef, self.entropy_coef, self.learning_rate, self.max_grad_norm)
 
         self.file_metrics = './models/metrics.csv'
         self.file_winrates = './models/winrates.csv'
@@ -69,43 +69,29 @@ class Trainer(object):
     def run(self):
         config.verbose = {key: False for key in config.verbose}
 
-        self.policy.base.train()
-
         for eps in range(self.episodes + 1):
 
             full_games_counter = 0
-            storages1 = [Storage(20000, config.state_space, config.action_space) for _ in range(3)]
-            storages2 = [Storage(20000, config.state_space, config.action_space) for _ in range(3)]
-
-            for s in storages1:
-                s.to(config.device)
-
-            for s in storages2:
-                s.to(config.device)
 
             game_copy = None
 
-            for n_game in range(self.n_games):
+            storage = self.storage_class()
+            print('---STARTING SIMULATIONS')
+            for n_game in tqdm(range(self.n_games)):
 
-                self.policy.update_decay()
 
-                self.policy.base.eval()
-                self.policy.use_decay = True
-
-                print('---GAME {} / {}'.format(n_game, self.n_games))
-
-                # n_fixed_agents = randint(1, 2)
-                # n_rl_agents = randint(1, 2)
                 n_fixed_agents = 1
                 n_rl_agents = 1
                 players = []
-                rl_agents = [Player(policy=self.policy, player_id=str(idx) + '_rl', storage=storages1[idx]) for idx in range(n_rl_agents)]
+
+                rl_agents = [Player(policy=self.policy, player_id=str(idx) + '_rl', storage=storage) for idx in range(n_rl_agents)]
+
                 fixed_agents = [Player(policy=FixedAgent(high=randint(300, 400), low=randint(100, 200), jail=randint(50, 150)),
-                                       player_id=str(idx) + '_fixed', storage=storages2[idx]) for idx in range(n_fixed_agents)]
+                                       player_id=str(idx) + '_fixed', storage=self.storage_class()) for idx in range(n_fixed_agents)]
                 players.extend(rl_agents)
                 players.extend(fixed_agents)
                 shuffle(players)
-                print('----- Players: {} fixed, {} rl'.format(n_fixed_agents, n_rl_agents))
+                # print('----- Players: {} fixed, {} rl'.format(n_fixed_agents, n_rl_agents))
 
                 game = Game(players=players)
                 game_copy = game
@@ -114,7 +100,6 @@ class Trainer(object):
                     player.set_game(game, n_game)
 
                 for n_round in range(self.n_rounds):
-                # while True:
 
                     # TODO: change this, don't like three completely the same conditional statements
                     if not game.is_game_active():     # stopping rounds loop
@@ -124,7 +109,7 @@ class Trainer(object):
 
                     for player in game.players:
 
-                        player.reset_mortgage_buy()
+                        # player.reset_mortgage_buy()
 
                         if player.is_bankrupt:            # must change it. do it two times because some players can go bankrupt when must pay bank interest
                             game.remove_player(player)    # other player's mortgaged spaces
@@ -141,7 +126,7 @@ class Trainer(object):
 
                             player.optional_actions()
 
-                            player.reset_mortgage_buy()
+                            # player.reset_mortgage_buy()
 
                             game.dice.roll()
 
@@ -185,9 +170,6 @@ class Trainer(object):
             action_losses = []
             dist_entropies = []
 
-            self.policy.base.train()
-            self.policy.use_decay = False
-
             for player in game_copy.players:
                 if 'rl' in player.id:
                     self.update(player, value_losses, action_losses, dist_entropies)
@@ -211,7 +193,6 @@ class Trainer(object):
                                                     np.average(action_losses), np.median(action_losses), np.mean(rewards)))
 
             if eps % self.verbose_eval == 0:
-                self.policy.base.eval()
                 print('------Arena')
                 arena = Arena(n_games=self.n_eval_games, n_rounds=self.n_rounds, verbose=0)  # add 3 types of logging. 0 - only show win rates.
                 print('--------RL vs Random')
@@ -228,17 +209,12 @@ class Trainer(object):
 
             print('---Full games {} / {}'.format(full_games_counter, self.n_games))
 
-
     def update(self, player, value_losses, action_losses, dist_entropies):
-        player.storage.truncate()
-        player.storage.to(self.device)
-
-        player.policy.base.eval()
         with torch.no_grad():
-            next_value = player.policy.get_value(player.storage.obs[-1]).detach()
+            next_value = player.policy.get_value(player.storage.states[-1])
 
-        player.storage.compute_returns(next_value, self.discount, self.gae_coef)
-        value_loss, action_loss, dist_entropy = self.agent.update(player.storage)
+        player.storage.compute(next_value)
+        value_loss, action_loss, dist_entropy = self.optimizer.update(player.storage)
 
         value_losses.append(value_loss)
         action_losses.append(action_loss)